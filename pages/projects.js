62c9dd692a1f2d45660a8484885a609942b3bb80
Raphninefo <rsirvent@mail.sfsu.edu>
2025-02-04 20:39:39 -0800

    Update projects page with Quarter Countdown link

--- pages/projects.js
+++ pages/projects.js
@@ -1,23 +1,23 @@
-// pages/projects.js
+import React from 'react';
+
 export default function Projects() {
-    return (
-      <div>
-        <h1>Projects</h1>
-        <ul>
-          <li>
-            <strong>Raph's Quarter Countdown</strong> – A web application that counts down to a special event.
-            Visit it{" "}
-            <a 
-              href="http://raphsquartercountdown.xyz/" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              here
-            </a>.
-          </li>
-          {/* You can add more projects below */}
-        </ul>
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{ padding: '2rem', color: 'white', backgroundColor: 'black', textAlign: 'center' }}>
+      <h1>Projects</h1>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        <li style={{ margin: '1rem 0' }}>
+          <strong>Raph's Quarter Countdown</strong> – A web application that counts down to a special event.
+          Visit it{' '}
+          <a 
+            href="http://raphsquartercountdown.xyz/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            style={{ color: '#61dafb', textDecoration: 'underline' }}
+          >
+            here
+          </a>.
+        </li>
+      </ul>
+    </div>
+  );
+}
